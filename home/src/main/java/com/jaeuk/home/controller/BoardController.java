8e53738117f31a75335c54e53b092727e1f3ecee
SimJaeuk <jaehook293@gmail.com>
2021-06-01 16:33:14 +0900

    게시판 등록, 수정, 데이터베이스 연동

--- home/src/main/java/com/jaeuk/home/controller/BoardController.java
+++ home/src/main/java/com/jaeuk/home/controller/BoardController.java
@@ -1,16 +1,15 @@
 package com.jaeuk.home.controller;
 
 import com.jaeuk.home.model.Board;
+import com.jaeuk.home.model.User;
 import com.jaeuk.home.repository.BoardRepository;
+import com.jaeuk.home.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -21,18 +20,31 @@ public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
+//    private UserRepository userRepository;
 
     @GetMapping("/list")
     public String list(Model model){
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards",boards);
+
+//        List<User> usernames = userRepository.findAll();
+//        model.addAttribute("usernames",usernames);
+
         return "board/list";
     }
 
+
     @GetMapping("/form")
-    public String form(Model model){
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if(id == null) {
+            model.addAttribute("board", new Board());
+        }else{
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
         return "board/form";
     }
+
 
     @PostMapping("/form")
     public String postForm(@Valid Board board, BindingResult bindingResult, Authentication authentication) {
