0b4054addd7a2163532a42b54d48f98f07bab6d8
SimJaeuk <jaehook293@gmail.com>
2021-06-10 17:22:31 +0900

    .

--- home2/src/main/java/com/jaeuk/home2/model/Board.java
+++ home2/src/main/java/com/jaeuk/home2/model/Board.java
@@ -3,10 +3,19 @@ package com.jaeuk.home2.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Data
 public class Board {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
+
 }
--- home2/src/main/java/com/jaeuk/home2/repository/BoardRepository.java
+++ home2/src/main/java/com/jaeuk/home2/repository/BoardRepository.java
@@ -3,5 +3,12 @@ package com.jaeuk.home2.repository;
 import com.jaeuk.home2.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
+
+
 }
